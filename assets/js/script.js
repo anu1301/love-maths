d3b27e86d02682b10bec4914a7d2990b8bf7d7c4
Anu Sethi <anu1301@hotmail.co.uk>
2022-01-19 19:45:09 +0000

    Add division game and finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    document.getElementById("answer-box").addEventListner("keydown", function (event) {
-        if (event.key === "Enter") {
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Return") {
             checkAnswer();
         }
     })
@@ -47,11 +47,12 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
 
-    } else if (gameType === "divisiom") {
+    } else if (gameType === "division") {
         displayDivisionQuestion(num1, num2);
+        
     } else {
         alert(`Unknown game type: ${gameType}`);
-        throw (`Unknown game type: ${gameType}.Aborting!`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
 
     }
 }
@@ -103,6 +104,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "/") {
 
         return [operand1 / operand2, "division"];
+    
     
     }else {
         alert(`unimplemented operator ${operator}`);
@@ -156,9 +158,11 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
 
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
+    
 
 }
